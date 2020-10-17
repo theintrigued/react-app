45d7eddf4cb789e2b5aa6d06e173a38a140fc34f
theintrigued <58659311+theintrigued@users.noreply.github.com>
2020-10-17 21:29:40 +0300

    Minor App and JSON file chages

--- my-monorepo/packages/app/App.tsx
+++ my-monorepo/packages/app/App.tsx
@@ -1,43 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-
-
-const App = () => {
-
-const [] = useState(0);
-  const [count, setCount] = useState(0);
-
-return (
 
 
 
-<View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>{count}</Text>
-      <Button title="increment" onPress={() => setCount(count +1)} />
-
-
-
-
-    </View>
-
-
-
-
-)
-
-
-}
+import {App} from '@wow/common';
 
 export default App
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
